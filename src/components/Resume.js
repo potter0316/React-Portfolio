83833863d1c79559812068b7c442a1dc1920744e
potter0316 <117317671+potter0316@users.noreply.github.com>
2023-05-22 17:24:17 -0500

    changes to portfolio

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import Moonfish from '../images/moonfish'
-// import Plentyofguilds form '../images/plentyofGuilds'
+import Moonfish from '../images/moonfish.png';
+import Plentyofguilds from '../images/plentyofGuilds.png';
 // import project3 from '../images'
 
 import '../styles/style.css';
@@ -8,10 +8,33 @@ import '../styles/style.css';
 function Resume() {
     return (
       <div class="resume">
-        <h3>Projects</h3>
+        <h2>Projects</h2>
+        <h3>Project 1: MoonFish</h3>
+        <div class="MoonfishImg">
+          <a href="https://github.com/lkalliance/Moon-Fish">
+            <img src={Moonfish} alt="moonfish">
+            </img>
+          </a>
+        </div>
+
+        <h3>Project 2: Plenty o guilds</h3>
+        <div class="PlentyofguildsImg">
+          <a href="https://github.com/lkalliance/Moon-Fish">
+            <img src={Plentyofguilds} alt="plenty of guilds">
+            </img>
+          </a>
+        </div>
+
+        <h3>Project 3: Coming Soon</h3>
+        <div class="PlentyofguildsImg">
+          <a href="https://github.com/lkalliance/Moon-Fish">
+            <img src={Plentyofguilds} alt="plenty of guilds">
+            </img>
+          </a>
+        </div>
         
         <hr />
-        <h3>Skills</h3>
+        <h2>Skills</h2>
         <ul>
             <li>HTML, CSS, Javascript</li> 
             <li>Git</li>
